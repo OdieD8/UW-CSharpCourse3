e361030f9ba93953772e99837a5d2659b1993706
OdieD8 <odie_de_moura@hotmail.com>
2017-04-10 21:56:06 -0600

    First commit

---
+++ HelloWorld/HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloWorld.Models;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RsvpForm(Models.GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Product()
+        {
+            var myProduct = new Product
+            {
+                ProductId = 1,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Category = "water-sports",
+                Price = 200m,
+            };
+
+            return View(myProduct);
+        }
+
+        public ActionResult Products()
+        {
+            var products = new Product[]
+            {
+                new Product{ ProductId = 1, Name = "First One", Price = 1.11m, ProductCount = 5 },
+                new Product{ ProductId = 2, Name = "Second One", Price = 2.22m, ProductCount = 1 },
+                new Product{ ProductId = 3, Name = "Third One", Price = 3.33m, ProductCount = 0 },
+                new Product{ ProductId = 4, Name = "Fourth One", Price = 4.44m, ProductCount = 2 },
+            };
+
+            return View(products);
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/Models/GuestResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace HelloWorld.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage="Please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please enter your e-mail")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please tell us if you will attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
