ab87e7a3d0cfc84254e9f1f62214d421bce4dda3
OdieD8 <odie_de_moura@hotmail.com>
2017-10-12 00:44:30 -0600

    Drop class, duplicate class add, confirmation, errors implemented

--- LearningCenter/LearningCenter/Controllers/ClassesController.cs
+++ LearningCenter/LearningCenter/Controllers/ClassesController.cs
@@ -21,6 +21,11 @@ namespace LearningCenter.Controllers
         [HttpGet]
         public ActionResult Enroll()
         {
+            if (Session == null || Session["User"] == null)
+            {
+                return RedirectToAction("Login", "Auth");
+            }
+
             List<string> list = new List<string>();
             foreach (var item in db.ClassDbContext.ToList())
             {
@@ -37,6 +42,11 @@ namespace LearningCenter.Controllers
         [HttpPost]
         public ActionResult Enroll(Models.Class value)
         {
+            if (Session == null || Session["User"] == null)
+            {
+                return RedirectToAction("Login", "Auth");
+            }
+
             if (value.SelectedClass[0].ToString() == "")
             {
                 ModelState.AddModelError("", "Please select a class");
@@ -47,18 +57,17 @@ namespace LearningCenter.Controllers
             var classIdquery = db.ClassDbContext.SqlQuery($"SELECT * FROM dbo.Class WHERE ClassName = '{selectedValue}'").ToList();
             int classId = Convert.ToInt16(classIdquery[0].ClassId);
 
-            if (Session == null || Session["User"] == null)
+            int userId = GetUserId();
+
+            try
             {
-                AuthController authController = new AuthController();
-                Session["User"] = authController.currentUserCookie;
+                db.Database.ExecuteSqlCommand($"INSERT INTO dbo.UserClass (ClassId, UserId) VALUES ({classId}, {userId})");
             }
-            string userEmail = Session["User"].ToString();
-            var userIdQuery = db.UserDbContext.SqlQuery($"SELECT * FROM [learning-center].[dbo].[User] WHERE UserEmail = '{userEmail}'").ToList();
-            int userId = Convert.ToInt16(userIdQuery[0].UserId);
-
-            db.Database.ExecuteSqlCommand($"INSERT INTO dbo.UserClass (ClassId, UserId) VALUES ({classId}, {userId})");
-
-
+            catch (Exception e)
+            {
+                TempData["errorMessage"] = "You are already enrolled in this class";
+                return RedirectToAction("Error", "Classes");
+            }
 
             return View("EnrolledClasses", db.UserDbContext.Where(u => u.UserId == userId).ToList());
         }
@@ -69,15 +78,52 @@ namespace LearningCenter.Controllers
         {
             if (Session == null || Session["User"] == null)
             {
-                AuthController authController = new AuthController();
-                Session["User"] = authController.currentUserCookie;
+                return RedirectToAction("Login", "Auth");
             }
+
+            int userId = GetUserId();
+            return View(db.UserDbContext.Where(u => u.UserId == userId).ToList());
+        }
+
+        // POST: Drop Class
+        [HttpPost]
+        public ActionResult EnrolledClasses(FormCollection collection)
+        {
+            if (Session == null || Session["User"] == null)
+            {
+                return RedirectToAction("Login", "Auth");
+            }
+
+            int userId = GetUserId();
+            string collectionValue = collection.Get("classId").ToString().Trim();
+            int collectionValueLength = collectionValue.Length;
+            int classId = Convert.ToInt16(collectionValue.Substring(0, collectionValueLength - 1));
+
+            try
+            {
+                db.Database.ExecuteSqlCommand($"DELETE FROM dbo.UserClass WHERE ClassId = {classId} AND UserId = {userId}");
+            }
+            catch (Exception e)
+            {
+                TempData["errorMessage"] = "Error dropping class";
+                return RedirectToAction("Error", "Classes");
+            }
+
+            return View("EnrolledClasses", db.UserDbContext.Where(u => u.UserId == userId).ToList());
+        }
+
+        private int GetUserId()
+        {
             string userEmail = Session["User"].ToString();
             var userIdQuery = db.UserDbContext.SqlQuery($"SELECT * FROM [learning-center].[dbo].[User] WHERE UserEmail = '{userEmail}'").ToList();
             int userId = Convert.ToInt16(userIdQuery[0].UserId);
+            return userId;
+        }
 
-
-            return View(db.UserDbContext.Where(u => u.UserId == userId).ToList());
+        [HttpGet]
+        public ActionResult Error()
+        {
+            return View();
         }
     }
 }
